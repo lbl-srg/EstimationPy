be10a0b1102c2cdac10ebab69ef24dc1594282e6
mbonvini <mbonvini@lbl.gov>
2013-09-18 08:48:14 -0700

    updated the package, the examples are working. To be verified and check the augmented version...

--- UKFpython/src/examples/ukfNonLinearClassic.py
+++ UKFpython/src/examples/ukfNonLinearClassic.py
@@ -1,38 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from   pylab import figure
 
-import sys
-sys.path.insert(0,'./models/stateParamEstimation')
-sys.path.insert(0,'./models/stateParamEstimation/plotting')
-sys.path.insert(0,'./ukf')
+from models.nonlinearClassic.model import model
+from models.nonlinearClassic.plotting import plotResults
 
-from model import *
-from ukf import *
-from ukfAugmented import *
-from plotting import *
+from ukf.ukf import ukf
+#from ukf.ukf import ukf_Augmented
 
 # time frame of the experiment
-dt = 0.02
+dt = 1
 startTime = 0.0
-stopTime  = 30.0
+stopTime  = 100.0
 numPoints = int((stopTime - startTime)/dt)
 time = np.linspace(startTime, stopTime, numPoints)
 
-# initial state vector (state x, par b)
-X0 = np.array([3.5, 5.0])
+# initial state vector (position, velocity)
+X0 = np.array([10.0])
 
-# output measurement covariance noise
-# R     = np.diag([0.1**2])
-R     = np.diag([0.2**2])
+# measurement covariance noise
+R  = np.diag([1.0])
 
-# input measurement covariance noise
-H     = np.diag([0.01*2])
-sqrtH = np.linalg.cholesky(H)
-
-# initial process noise
-Q     = np.diag([0.05**2, 0.2**2])
-#Q     = np.diag([1, 1])
+# process noise
+Q  = np.diag([1.0])
 
 # define the model
 m = model()
@@ -42,9 +30,7 @@ m.setQ(Q)
 m.setR(R)
 
 # input vector
-U = np.ones((numPoints,1))
-# measured input vector
-Um = np.ones((numPoints,1))
+U = np.zeros((numPoints,1))
 
 # initialize state and output vectors
 n_state   = m.getNstates()
@@ -54,19 +40,19 @@ X = np.zeros((numPoints,n_state))
 Y = np.zeros((numPoints,n_outputs))
 Z = np.zeros((numPoints,n_outputs))
 
+
 # evolution of the system
 for i in range(numPoints):
 	if i==0:
 		X[i,:] = X0
-	else:	
-		X[i,:]= m.functionF(X[i-1,:],U[i-1,:],time[i-1])
+	else:
+		values = (X[i-1,:], U[i-1,:], U[i,:], time[i-1], time[i], True) 
+		X[i,:]= m.functionF(values) + np.dot(m.sqrtR, np.random.randn(1,n_state))
 
 	Y[i,:]  = m.functionG(X[i,:],U[i,:],time[i])
 
 # noisy measured values
-Z  = Y + np.dot(m.sqrtR, np.random.randn(n_outputs,numPoints)).T
-Um = U + np.dot(sqrtH, np.random.randn(1,numPoints)).T
-
+Z = Y + np.dot(m.sqrtR, np.random.randn(n_outputs,numPoints)).T
 
 ########################################################################################
 # THE NOISY MEASUREMENTS OF THE OUTPUTS ARE AVAILABLE, START THE FILERING PROCEDURE
@@ -78,27 +64,27 @@ S    = np.zeros((numPoints,n_state,n_state))
 Sy   = np.zeros((numPoints,n_outputs,n_outputs))
 
 # initial knowledge
-X0_hat = np.array([2.5, 2.0])
+X0_hat = np.array([11.0])
 S0     = m.sqrtQ
 
 # UKF parameters
-UKFilter  = ukf(n_state,n_outputs)
+UKFilter  = ukf(n_state, n_state, n_outputs)
 
 # iteration of the UKF
 for i in range(numPoints):
 	
 	if i==0:	
-		Xhat[i,:] = X0_hat
-		S[i,:,:]  = S0
-		Yhat[i,:] = m.functionG(Xhat[i,:],U[i,:],time[i])
-		Sy[i,:,:] = m.sqrtR
+		Xhat[i,:]   = X0_hat
+		S[i,:,:]    = S0
+		Yhat[i,:]   = m.functionG(Xhat[i,:],U[i,:],time[i])
+		Sy[i,:,:]   = m.sqrtR
 	else:
-		Xhat[i,:], S[i,:,:], Yhat[i,:], Sy[i,:,:] = UKFilter.ukf_step(Z[i],Xhat[i-1,:],S[i-1,:,:],m.sqrtQ,m.sqrtR,Um[i-1,:],Um[i,:],time[i-1],time[i],m,False)
+		Xhat[i,:], S[i,:,:], Yhat[i,:], Sy[i,:,:] = UKFilter.ukf_step(Z[i],Xhat[i-1,:],S[i-1,:,:],m.sqrtQ,m.sqrtR,U[i-1,:],U[i,:],time[i-1],time[i],m,False)
 
 # smoothing the results
 Xsmooth = np.zeros((numPoints,n_state))
 Ssmooth = np.zeros((numPoints,n_state,n_state))
-Xsmooth, Ssmooth = UKFilter.smooth(time,Xhat,S,m.sqrtQ,Um,m)
+Xsmooth, Ssmooth = UKFilter.smooth(time,Xhat,S,m.sqrtQ,U,m)
 
 # converting the squared matrix into the covariance ones
 P         = np.zeros(S.shape)
@@ -120,37 +106,39 @@ S_Aug    = np.zeros((numPoints,n_state,n_state))
 Sy_Aug   = np.zeros((numPoints,n_outputs,n_outputs))
 
 # initial knowledge
-X0_hat_Aug = np.array([2.5, 2.0])
+X0_hat_Aug = np.array([12.0])
 Q0         = Q
 R0         = R
 Sq0        = np.linalg.cholesky(Q0)
 Sr0        = np.linalg.cholesky(R0)
 
 # UKF parameters
-UKFilter_Aug  = ukfAugmented(n_state,n_outputs)
-UKFilter_Aug.setAugmentedPars(0.995, 1.0/np.sqrt(3.0), 0.5*np.diag(np.ones(n_state)))
+#UKFilter_Aug  = ukfAugmented(n_state,n_outputs)
+#UKFilter_Aug.setAugmentedPars(0.995, 1.0/np.sqrt(3.0), 0.1*np.diag(np.ones(n_state)))
+
 
 # iteration of the UKF
-for i in range(numPoints):
-	
-	if i==0:	
-		Xhat_Aug[i,:] = X0_hat_Aug
-		S_Aug[i,:,:]  = Sq0
-		Yhat_Aug[i,:] = m.functionG(Xhat_Aug[i,:],U[i,:],time[i])
-		Sy_Aug[i,:,:] = Sr0
-		Sq            = Sq0
-		alpha_s       = 0.005
-	else:
-		Xhat_Aug[i,:], S_Aug[i,:,:], Yhat_Aug[i,:], Sy_Aug[i,:,:], Sq, alpha_s = UKFilter_Aug.ukfAugmented_step(Z[i],Xhat_Aug[i-1,:],S_Aug[i-1,:,:],Sq,Sr0,alpha_s,Um[i-1,:],Um[i,:],time[i-1],time[i],m,False)
+#for i in range(numPoints):
+#	
+#	if i==0:	
+#		Xhat_Aug[i,:] = X0_hat_Aug
+#		S_Aug[i,:,:]  = Sq0
+#		Yhat_Aug[i,:] = m.functionG(Xhat_Aug[i,:],U[i,:],time[i])
+#		Sy_Aug[i,:,:] = Sr0
+#		Sq            = Sq0
+#		alpha_s       = 0.005
+#	else:
+#		Xhat_Aug[i,:], S_Aug[i,:,:], Yhat_Aug[i,:], Sy_Aug[i,:,:], Sq, alpha_s = UKFilter_Aug.ukfAugmented_step(Z[i],Xhat_Aug[i-1,:],S_Aug[i-1,:,:],Sq,Sr0,alpha_s,U[i-1,:],U[i,:],time[i-1],time[i],m,False)
 
 # converting the squared matrix into the covariance ones
 P_Aug         = np.zeros(S.shape)
 covY_Aug      = np.zeros(Sy.shape)
-(N, I, J)     = P_Aug.shape
-for n in range(N):
-	P_Aug[n,:,:]       = np.dot(S_Aug[n,:,:],S_Aug[n,:,:].T)
-	covY_Aug[n,:,:]    = np.dot(Sy_Aug[n,:,:], Sy_Aug[n,:,:].T)
+#(N, I, J)     = P_Aug.shape
+#for n in range(N):
+#	P_Aug[n,:,:]       = np.dot(S_Aug[n,:,:],S_Aug[n,:,:].T)
+#	covY_Aug[n,:,:]    = np.dot(Sy_Aug[n,:,:], Sy_Aug[n,:,:].T)
 
 ###############################################################################################
 # plot the results
-plotResults(time,stopTime,X,Y,Z,U,Um,Xhat,Yhat,P,covY,Xsmooth,Psmooth,Xhat_Aug,P_Aug)
+plotResults(time,stopTime,X,Y,Z,Xhat,Yhat,P,covY,Xsmooth,Psmooth,Xhat_Aug,P_Aug)
+
--- UKFpython/src/models/nonlinearClassic/model.py
+++ UKFpython/src/models/nonlinearClassic/model.py
@@ -1,6 +1,7 @@
 import numpy as np
+from ukf.Model import Model
 
-class model():
+class model(Model):
 	# parameters of the model
 	a  = 0.5
 	b  = 25
@@ -12,7 +13,7 @@ class model():
 	# measurement covariance noise
 	R     = np.array([[1.0]])
 	sqrtR = np.linalg.cholesky(R)
-	 
+	
 	# initial state vector (position, velocity)
 	X0 = np.array([1.0])
 
@@ -28,52 +29,24 @@ class model():
 	Initialize the model with parameters
 	"""
 	def __init__(self, a = 0.5, b  = 25, c  = 8.0, d = 0.05, dt = 0.1, X0 = np.array([1.0])):
+		# use the initialization method of inherited class
+		Model.__init__(self, 1, 1, 1, X0, None, [1.0], [1.0])
+		
+		# set parameters of the model
+		self.setPars(a, b, c, d, dt)
+
+	"""
+	This method modify the parameters of the model
+	"""
+	def setPars(self, a, b, c, d, dt):
 		# parameters of the model
-		self.a = a
-		self.b = b
-		self.dt = dt
 		self.pars = (a, b, c, d, dt)
-	
-		# measurement covariance noise
-		self.R     = np.array([[0.004]])
-		self.sqrtR =  np.linalg.cholesky(self.R)
-		 
-		# initial state vector (position, velocity)
-		self.X0 = X0
-
-		# process noise
-		self.Q     = np.array([[0.01]])
-		self.sqrtQ = np.linalg.cholesky(self.Q)
-	
-		# initialize state and output vectors
-		n_states  = 1
-		n_outputs = 1
-
-	"""
-	This method define the initial state
-	"""
-	def setInitialState(self,X0):
-		self.X0 = X0	
 
 	"""
 	This method initialize the sampling step
 	"""
 	def setDT(self,dt=0.1):
 		self.dt = dt
-
-	"""
-	This method initialize the state noise matrix
-	"""
-	def setQ(self,Q):
-		self.Q     = Q
-		self.sqrtQ = np.linalg.cholesky(Q)
-
-	"""
-	This method initialize the measurement noise matrix
-	"""
-	def setR(self,R):
-		self.R     = R
-		self.sqrtR = np.linalg.cholesky(R)		
 
 	"""
 	State evolution function
@@ -86,7 +59,9 @@ class model():
 
 	* new state at time t+1
 	"""
-	def functionF(self,x,u,t,simulate=True):
+	def functionF(self, val):
+		(x,u_old,u,t_old,t,simulate) = val
+		
 		# parameters of the function
 		a, b, c, d, dt = self.pars
 		x_old = x[0]
@@ -119,15 +94,3 @@ class model():
 
 		# return the output
 		return np.array([y])
-	
-	"""
-	get the number of states variables
-	"""
-	def getNstates(self):
-		return self.n_states
-
-	"""
-	get the number of states variables
-	"""
-	def getNoutputs(self):
-		return self.n_outputs
