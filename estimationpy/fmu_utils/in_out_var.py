93a47a74eebad563adc135c481bcfea9839423fa
Marco Bonvini <bonvini.m@gmail.com>
2015-08-28 13:07:55 -0700

    Continuing documentation effort

--- estimationpy/fmu_utils/in_out_var.py
+++ estimationpy/fmu_utils/in_out_var.py
@@ -35,16 +35,27 @@ class InOutVar():
     
     """
     
-    def __init__(self, object = None):
+    def __init__(self, pyfmi_var = None):
         """
-        Initialization method. This constructor defines the object (PyFmiVariable) that contains the
-        information about the variable, the CsvReader class associated to the input (that contains the data), 
+        Consttuctor for the class :class:`InOutVar`. The constructor takes as input
+        parameter an object of type **pyfmi.fmi.ScalarVariable** that contains the
+        information about the variable.
+        The method initializes an empty **CsvReader** object and an empty **pandas.Series**
+        object that can be futher defined.
+        The method also initializes the covariance :math:`\sigma^2` associated to this variable to be 1.0,
+        and the flag ``measOut = False``.
+        
+        The class provide, the CsvReader class associated to the input (that contains the data), 
         a dictionary called dataSeries = {"time": [], "data": []} that contains the two arrays that represent
         the data series (that are read from the csv file).
+        
         The integer index is used when the data values are read using the function ReadFromDataSeries, while
         cov is the covariance associated to the data series.
+
+        :param pyfmi.fmi.ScalarVariable pyfmi_var: the pyfmi object representing a variable.
+        
         """
-        self.object = object
+        self.pyfmi_var = pyfmi_var
         self.csvReader = CsvReader()
         self.dataSeries = pd.Series()
         
@@ -54,19 +65,29 @@ class InOutVar():
     
     def read_value_in_fmu(self, fmu):
         """
-        Given an FMU model, this method reads the value of the variable/parameter
+        This method reads the value of a variable/parameter 
+        assumes in a specific FMU object.
+        
+        :param FmuModel fmu: an object representing an FMU model in PyFMI.
+        
+        :return: The value of the variable represented by an instance of this class.
+          The method returns `None` is the type of the variable is not recognized as one
+          of the available ones (Real, Integer, Boolean, Enumeration, String).
+        
+        :rtype: float, None
+        
         """
-        type = self.object.type
+        type = self.pyfmi_var.type
         if type == pyfmi.fmi.FMI_REAL:
-            val = fmu.get_real(self.object.value_reference)
+            val = fmu.get_real(self.pyfmi_var.value_reference)
         elif type == pyfmi.fmi.FMI_INTEGER:
-            val = fmu.get_integer(self.object.value_reference)
+            val = fmu.get_integer(self.pyfmi_var.value_reference)
         elif type == pyfmi.fmi.FMI_BOOLEAN:
-            val = fmu.get_boolean(self.object.value_reference)
+            val = fmu.get_boolean(self.pyfmi_var.value_reference)
         elif type == pyfmi.fmi.FMI_ENUMERATION:
-            val = fmu.get_int(self.object.value_reference)
+            val = fmu.get_int(self.pyfmi_var.value_reference)
         elif type == pyfmi.fmi.FMI_STRING:
-            val = fmu.get_string(self.object.value_reference)
+            val = fmu.get_string(self.pyfmi_var.value_reference)
         else:
             print "OnSelChanged::FMU-EXCEPTION :: The type is not known"
             return None
@@ -74,20 +95,38 @@ class InOutVar():
     
     def set_measured_output(self, flag = True):
         """
-        This method set the flag that indicates if the variable represents a measured output
+        This method set the flag that indicates if the variable represents a measured output.
+        
+        :param bool flag: flag that indicates whether this variable is a measured output
+          or not.
         """
         self.measOut = flag
     
     def is_measured_output(self):
         """
         This method returns the value of the boolean flag that describe
-        if the variable is a measured output
+        if the variable is a measured output.
+        
+        :return: a boolean value that indicates if this variable is a measured output.
+
+        :rtype: bool
+
         """
         return self.measOut
     
     def set_covariance(self, cov):
         """
-        This method sets the covariance associated to the input/output variable
+        This method sets the covariance associated to
+        an instance of the class :class:`InOutVar` that is used
+        by the state and parameter estimation algorithm.
+        
+        :param float cov: The value to be used as initial value in the estimation
+            algorithm. The value must be positive.
+        
+        :return: True if the value has been set corectly, False otherwise.
+        
+        :rtype: bool
+        
         """
         if cov > 0.0:
             self.cov = cov
@@ -98,28 +137,51 @@ class InOutVar():
     
     def get_covariance(self):
         """
-        This method returns the covariance of the variable
+        This method returns the covariance of the **InOutVar** object.
+        
+        :return: the covariance of the variable.
+        :rtype: float
+        
         """
         return self.cov
      
-    def set_object(self, obj):
+    def set_object(self, pyfmi_var):
         """
-        Set the object <<pyfmi.fmi.ScalarVariable>> associated to the input/output
+        This method sets the variable associated to the object of class
+        **InOutVar**.
+        
+        :param pyfmi.fmi.ScalarVariable pyfmi_var: The variable associated to the input/output represented
+          by the object.
+        
+        :raises TypeError: The method raises an exception if the parameter is not of the proper type.
+
         """
-        if isinstance(obj, pyfmi.fmi.ScalarVariable):
-            self.object = obj
+        if isinstance(pyfmi_var, pyfmi.fmi.ScalarVariable):
+            self.pyfmi_var = pyfmi_var
         else:
-            raise TypeError("The object passed to the method InOutVar.SetObject() is not of type pyfmi.fmi.ScalarVariable ")
+            raise TypeError("The object passed to the method InOutVar.set_object() is not of type pyfmi.fmi.ScalarVariable ")
         
     def get_object(self):
         """
-        Get the object <<pyfmi.ScalarVariable>> associated to the input/output
+        Get the object associated to the **pyfmi.ScalarVariable** associated to this input/output
+        variable.
+        
+        :return: the pyfmi variable object reference by this variable.
+        :rtype: pyfmi.fmi.ScalarVariable
+
         """
-        return self.object
+        return self.pyfmi_var
     
     def set_csv_reader(self, reader):
         """
-        Set the CsvReader class associated to the input/output
+        This method associates an object of type :class:`estimationpy.fmu_utils.csv_reader.CsvReader` to this
+        input/output variable. The **CsvReder** will be used to read data by the state and parameter estimation
+        algorithm.
+
+        :param estimationpy.fmu_utils.csv_reader.CsvReader reader: The **CsvReader** object to associate to this variable.
+        
+        :raises TypeError: The method raises an exception if the type of the argument reader is not correct.
+
         """
         if isinstance(reader, CsvReader):
             self.csvReader = reader
@@ -130,13 +192,27 @@ class InOutVar():
         
     def get_csv_reader(self):
         """
-        Get the CsvReader class associated to the input/output
+        This method returns a reference to the **CsvReader** object associated to the
+        input/output variable.
+        
+        :return: the reference to the **CsvReader** object.
+
+        :rtype: estimationpy.fmu_utils.csv_reader.CsvReader
+
         """
         return self.csvReader
     
     def read_data_series(self):
         """
-        This method reads the data series contained in the CSV file
+        This method reads the data series associated to this input/output variable.
+        The method checks if the **CsvReader** object has a valid file name 
+        associated to it. If not it looks if the data series has already been
+        specified and is a not empty **pandas.Series**.
+        The method returns True if the data is read, False otherwise.
+        
+        :return: True if the method is able to read the data from either the CsvReader obejct
+          of the pandas.Series. False otherwise.
+        :rtype: bool
         """
         # If the CsvReader has been specified the try to load the data from there
         if self.csvReader.filename == "" or self.csvReader.filename == None:
@@ -157,16 +233,28 @@ class InOutVar():
         
     def get_data_series(self):
         """
-        This method returns the data series read from the csv file
+        This method returns the data series associated to this input/output variable.
+        The dat aseries can be either specified by a CSV file by means of a
+        **Csvreader** object, or directly from a **pandas.Series** object.
+        
+        :return: the pandas.Series associated to this variable.
+        :rtype: pandas.Series
         """
         return self.dataSeries
     
     def set_data_series(self, series):
         """
         This function sets a data series instead of reading it from the CSV file.
-        The data series has to be formatted as a pandas series that has a 
-            pandas.tseries.index.DatetimeIndex
-        as index.
+        The data series has to be a **pandas.Series** object that is indexed with
+        a **pandas.tseries.index.DatetimeIndex**.
+        
+        :param pandas.Series series: the time series to be associated to this input/output
+          variable.
+        
+        :raises TypeError: The method raise an error if the parameter ``series`` is
+          not of the right type or it's a pandas.Series not indexed with a 
+          **pandas.tseries.index.DatetimeIndex**.
+
         """
         if isinstance(series, pd.Series):
             if isinstance(series.index, pd.DatetimeIndex):
@@ -178,9 +266,24 @@ class InOutVar():
         
     def read_from_data_series(self, ix, verbose = False):
         """
-        This function given a datetime index returns the value of the data series at that point.
-        The type of ix is a Timestamp.
+        This method reads and return the value associated to the input/output variable
+        at the time specified by the parameter ``ix``. The parameter ``ix`` needs
+        to be a vaid index that belongs to a **pandas.tseries.index.DatetimeIndex**.
+        If the index ``ix`` is not one of the indexes of the pandas.Series the
+        method performs a linear interpolation between the two closest values to 
+        compute the value.
+        
+        :param ix: the time stamp for which providing the value.
+        :param bool verbose: boolean parameter that prints verbose log messages during
+          the interpolation process. Use only for debugging/diagnostic purposes.
+
+        :return: the value that is read from the pandas.Series associated to the variable.
+          if the index is out of range the method returns False.
+        
+        :rtype: float, bool
+        
         """
+        # Identify start and end date of the period covered by the time series
         from_start = (ix - self.dataSeries.index[0]).total_seconds()
         to_end = (self.dataSeries.index[-1] - ix).total_seconds()
         
